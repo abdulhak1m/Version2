7fdaf55206aa00eba3a39ce15d0d3a505e8d036e
komok113 <abdulhakim.dev@gmail.com>
2019-10-02 21:21:58 +0300

    SecondVersion

---
+++ TextToSpeechDemo/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TextToSpeechDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(28, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(760, 338);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Speak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(314, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 55);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Resume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(483, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 29);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ TextToSpeechDemo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech;
+
+namespace TextToSpeechDemo
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speech;
+        public Form1()
+        {
+            InitializeComponent();
+            speech = new SpeechSynthesizer();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                speech.SelectVoice(comboBox1.Text);
+                speech.SpeakAsync(richTextBox1.Text);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (speech.State == SynthesizerState.Speaking)
+            {
+                speech.Pause();
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (speech.State == SynthesizerState.Paused)
+            {
+                speech.Resume();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var voice in speech.GetInstalledVoices())
+            {
+                comboBox1.Items.Add(voice.VoiceInfo.Name);
+            }
+                  
+
+        }
+    }
+}
